205936862d1e98a650e49af908e022a1bfffa638
devpuraanjali <devpura.anjali@gmail.com>
2015-01-24 11:33:10 -0800

    Second checkin

---
+++ src/com/devpura/anjali/codingbat/HelloAbhi.java
@@ -0,0 +1,10 @@
+package com.devpura.anjali.codingbat;
+
+/**
+ * Created by anjali on 1/24/15.
+ */
+public class HelloAbhi {
+    public static void main(String[] args) {
+        System.out.println("Hello Abhi");
+    }
+}
