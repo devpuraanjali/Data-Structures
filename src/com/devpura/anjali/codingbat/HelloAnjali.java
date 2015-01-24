abb7ab0fc5790d5801b78dbae9e29bc45d3e65d3
devpuraanjali <devpura.anjali@gmail.com>
2015-01-24 11:31:14 -0800

    First Git checkin

---
+++ src/com/devpura/anjali/codingbat/HelloAnjali.java
@@ -0,0 +1,10 @@
+package com.devpura.anjali.codingbat;
+
+/**
+ * Created by anjali on 1/24/15.
+ */
+public class HelloAnjali {
+    public static void main(String[] args) {
+        System.out.println("ANjali is Home");
+    }
+}
---
+++ src/com/devpura/anjali/codingbat/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.devpura.anjali.codingbat;
+
+/**
+ * Created by anjali on 1/24/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
